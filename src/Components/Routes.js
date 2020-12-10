25775911c12c9cea690464cae56eba8b23707cf4
Christopher Barnes <chris.barnes.2000@me.com>
2020-12-10 00:49:30 -0800

    [Update]: Routes

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,5 +1,5 @@
 export const NOT_FOUND = '*';
-export const HOME = "/RobinWilliams";
-export const LANDING = "/RobinWilliams";
-export const WORK_CITED = "/RobinWilliams/Work_Cited";
-export const PBS_VIDEOS = "/RobinWilliams/PBS_Videos";
+export const HOME = "/";
+export const LANDING = "/";
+export const WORK_CITED = "/Work_Cited";
+export const PBS_VIDEOS = "/PBS_Videos";

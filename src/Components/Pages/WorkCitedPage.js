c84d2ea0ee75dc760613404e0cfa8763e8569c9b
Christopher Barnes <chris.barnes.2000@me.com>
2020-12-10 23:33:11 -0800

    [Update]: Work Cited

--- src/Components/Pages/WorkCitedPage.js
+++ src/Components/Pages/WorkCitedPage.js
@@ -12,122 +12,154 @@ class WorkCitedPage extends Component {
           <p>
             "ABI Community: Social Justice Youth Program". Abicommunity.Org, 2020,
             <br />
-            https://www.abicommunity.org/programs/youth_program/about_social_justice_youth_program.html.
+            <a href="https://www.abicommunity.org/programs/youth_program/about_social_justice_youth_program.html">
+              "https://www.abicommunity.org/programs/youth_program/about_social_justice_youth_program.html"
+            </a>
             <br />
-            Accessed 11 Nov 2020.
+            .Accessed 11 Nov 2020.
           </p>
           <p>
             "A Tribute To Robin Williams: Comedian, Actor, And Philanthropist - Collin County Democrats". Collin County
             Democrats, 2014,
             <br />
-            https://www.collindemocrats.org/a-tribute-to-robin-williams-comedian-actor-and-philanthropist/.
+            <a href="https://www.collindemocrats.org/a-tribute-to-robin-williams-comedian-actor-and-philanthropist/">
+              "https://www.collindemocrats.org/a-tribute-to-robin-williams-comedian-actor-and-philanthropist/"
+            </a>
             <br />
-            Accessed 15 Nov 2020.
+            .Accessed 15 Nov 2020.
           </p>
           <p>
             "Great Performances | Twilight: Los Angeles | Season 42 | Episode 23". PBS.Org, 2020,
             <br />
-            https://www.pbs.org/video/great-performances-twilight-los-angeles-1/.
+            <a href="https://www.pbs.org/video/great-performances-twilight-los-angeles-1/">
+              "https://www.pbs.org/video/great-performances-twilight-los-angeles-1/
+            </a>
             <br />
-            Accessed 11 Nov 2020.
+            .Accessed 11 Nov 2020.
           </p>
           <p>
             "How (Not) ToSignal “Stop” | Savage Minds". Savageminds.Org, 2008,
             <br />
-            https://savageminds.org/2008/09/28/how-not-to-signal-stop/.
+            <a href="https://savageminds.org/2008/09/28/how-not-to-signal-stop/">
+              "https://savageminds.org/2008/09/28/how-not-to-signal-stop/"
+            </a>
             <br />
-            Accessed 11 Dec 2020.
+            .Accessed 11 Dec 2020.
           </p>
           <p>
             "Los Angeles Riots". HISTORY, 2020,
             <br />
-            https://www.history.com/topics/1990s/the-los-angeles-riots.
+            <a href="https://www.history.com/topics/1990s/the-los-angeles-riots">
+              "https://www.history.com/topics/1990s/the-los-angeles-riots"
+            </a>
             <br />
-            Accessed 16 Nov 2020.
+            .Accessed 16 Nov 2020.
           </p>
           <p>
             "Multimodal Projects". Kettering College Writing Center, 2020,
             <br />
-            https://kcwritingcenter.weebly.com/multimodal-projects.html.
+            <a href="https://kcwritingcenter.weebly.com/multimodal-projects.html">
+              "https://kcwritingcenter.weebly.com/multimodal-projects.html"
+            </a>
             <br />
-            Accessed 11 Nov 2020.
+            .Accessed 11 Nov 2020.
           </p>
           <p>
             "PBS Newshour | Robin Williams Didn’T Know He Had This Mysterious Disease | Season 2015". PBS.Org, 2020,
             <br />
-            "https://www.pbs.org/video/robin-williams-didn-t-know-he-had-this-mysterious-disease-1454024744/".
+            <a href="https://www.pbs.org/video/robin-williams-didn-t-know-he-had-this-mysterious-disease-1454024744/">
+              "https://www.pbs.org/video/robin-williams-didn-t-know-he-had-this-mysterious-disease-1454024744/"
+            </a>
             <br />
-            Accessed 7 Dec 2020.
+            .Accessed 7 Dec 2020.
           </p>
           <p>
             Robertson, Ray. "Can We All Get Along?" Blacks' Historical And Contemporary (In) Justice With Law
             Enforcement.". Academia.Edu, 2020,
             <br />
-            https://www.academia.edu/9014385/_Can_We_All_Get_Along_Blacks_Historical_and_Contemporary_In_Justice_with_Law_Enforcement.
+            <a href="https://www.academia.edu/9014385/_Can_We_All_Get_Along_Blacks_Historical_and_Contemporary_In_Justice_with_Law_Enforcement">
+              "https://www.academia.edu/9014385/_Can_We_All_Get_Along_Blacks_Historical_and_Contemporary_In_Justice_with_Law_Enforcement"
+            </a>
             <br />
-            Accessed 20 Nov 2020.
+            .Accessed 20 Nov 2020.
           </p>
           <p>
             "Rebecca Robbins, STAT. "How Lewy Body Dementia Gripped Robin Williams". Scientific American, 2016,
             <br />
-            "https://www.scientificamerican.com/article/how-lewy-body-dementia-gripped-robin-williams1/".
+            <a href="https://www.scientificamerican.com/article/how-lewy-body-dementia-gripped-robin-williams1/">
+              "https://www.scientificamerican.com/article/how-lewy-body-dementia-gripped-robin-williams1/"
+            </a>
             <br />
-            Accessed 8 Dec 2020.
+            .Accessed 8 Dec 2020.
           </p>
           <p>
             "Robin Williams: Come Inside My Mind | Kanopy". Dominican.Kanopy.Com, 2020,
             <br />
-            https://dominican.kanopy.com/video/robin-williams-come-inside-my-mind”.
+            <a href="https://dominican.kanopy.com/video/robin-williams-come-inside-my-mind">
+              "https://dominican.kanopy.com/video/robin-williams-come-inside-my-mind”
+            </a>
             <br />
-            Accessed 15 Nov 2020.
+            .Accessed 15 Nov 2020.
           </p>
           <p>
             "Robin Williams Remembered | Kanopy". Dominican.Kanopy.Com, 2020,
             <br />
-            "https://dominican.kanopy.com/video/robin-williams-remembered".
+            <a href="https://dominican.kanopy.com/video/robin-williams-remembered">
+              "https://dominican.kanopy.com/video/robin-williams-remembered"
+            </a>
             <br />
-            Accessed 15 Nov 2020.
+            .Accessed 15 Nov 2020.
           </p>
           <p>
             "Robin Williams And The Stigma Of Mental Illness". Pacific Standard, 2020,
             <br />
-            https://psmag.com/social-justice/much-stigma-mental-illness-harm-robin-williams-88589.
+            <a href="https://psmag.com/social-justice/much-stigma-mental-illness-harm-robin-williams-88589">
+              "https://psmag.com/social-justice/much-stigma-mental-illness-harm-robin-williams-88589"
+            </a>
             <br />
-            Accessed 15 Nov 2020.
+            .Accessed 15 Nov 2020.
           </p>
           <p>
             "Rodney King". Biography, 2020, <br />
-            https://www.biography.com/crime-figure/rodney-king.
+            <a href="https://www.biography.com/crime-figure/rodney-king">
+              "https://www.biography.com/crime-figure/rodney-king"
+            </a>
             <br />
-            Accessed 15 Nov 2020.
+            .Accessed 15 Nov 2020.
           </p>
           <p>
             "Rodney King". En.Wikipedia.Org, 2020,
             <br />
-            https://en.wikipedia.org/wiki/Rodney_King.
+            <a href="https://en.wikipedia.org/wiki/Rodney_King">"https://en.wikipedia.org/wiki/Rodney_King"</a>
             <br />
-            Accessed 15 Nov 2020.
+            .Accessed 15 Nov 2020.
           </p>
           <p>
             "Twilight: Los Angeles | About The Production | Great Performances | PBS". Great Performances, 2012,
             <br />
-            https://www.pbs.org/wnet/gperf/twilight-los-angeles-about-the-production/1329/.
+            <a href="https://www.pbs.org/wnet/gperf/twilight-los-angeles-about-the-production/">
+              "https://www.pbs.org/wnet/gperf/twilight-los-angeles-about-the-production/"
+            </a>
             <br />
-            Accessed 11 Nov 2020.
+            .Accessed 11 Nov 2020.
           </p>
           <p>
             "What Are Social Justice Issues? | Maryville Online". Maryville Online, 2019,
             <br />
-            https://online.maryville.edu/blog/what-are-social-justice-issues/.
+            <a href="https://online.maryville.edu/blog/what-are-social-justice-issues/">
+              "https://online.maryville.edu/blog/what-are-social-justice-issues/"
+            </a>
             <br />
-            Accessed 16 Nov 2020.
+            .Accessed 16 Nov 2020.
           </p>
           <p>
             "Why We Missed The Signs Of My Mom's Lewy Body Dementia". Next Avenue, 2016,
             <br />
-            "https://www.nextavenue.org/why-we-missed-the-signs-of-my-moms-lewy-body-dementia/?utm_source=PBS&utm_medium=web&utm_campaign=merlin".
+            <a href="https://www.nextavenue.org/why-we-missed-the-signs-of-my-moms-lewy-body-dementia/?utm_source=PBS&utm_medium=web&utm_campaign=merlin">
+              "https://www.nextavenue.org/why-we-missed-the-signs-of-my-moms-lewy-body-dementia/?utm_source=PBS&utm_medium=web&utm_campaign=merlin"
+            </a>
             <br />
-            Accessed 8 Dec 2020.
+            .Accessed 8 Dec 2020.
           </p>
           <button>
             <Link to={PBS_VIDEOS}>Agains, Please Watch These Videos</Link>

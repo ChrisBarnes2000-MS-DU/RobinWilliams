4f8d85a9c7e6e5ec6951f106cdf9a0ca7187d149
Christopher Barnes <chris.barnes.2000@me.com>
2020-12-11 00:17:36 -0800

    [Update]: style display

--- src/Components/Pages/PBS_VideosPage.js
+++ src/Components/Pages/PBS_VideosPage.js
@@ -7,137 +7,139 @@ import { HOME } from "../Routes";
 class PBS_VideosPage extends Component {
   render() {
     return (
-      <div className="add-padding-bottom pbs-videos-container">
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365305654/"
-          video_url="https://www.pbs.org/video/robin-williams-made-transformation-look-effortless-1415140089/"
-          site_url="https://www.pbs.org/video/robin-williams-made-transformation-look-effortless-1415140089/"
-          site_title="PBS NEWSHOUR: Robin Williams made transformation look effortless (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365509819/"
-          video_url="https://www.pbs.org/video/on-story-dead-poets-society-deconstructing-acclaimed-classic/"
-          site_url="https://www.pbs.org/video/on-story-dead-poets-society-deconstructing-acclaimed-classic/"
-          site_title="ON STORY Dead Poets Society: Deconstructing the Acclaimed Classic (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365308667/"
-          video_url="https://www.pbs.org/video/kqed-newsroom-education-reform-rim-fire-remembering-robin-williams/"
-          site_url="https://www.pbs.org/video/kqed-newsroom-education-reform-rim-fire-remembering-robin-williams/"
-          site_title="KQED NEWSROOM: Education Reform, Rim Fire, Remembering Robin Williams (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365839270/"
-          video_url="https://www.pbs.org/video/kqed-newsroom-police-sexual-misconduct-tech-sam-liccardo-robin-williams/"
-          site_url="https://www.pbs.org/video/kqed-newsroom-police-sexual-misconduct-tech-sam-liccardo-robin-williams/"
-          site_title="KQED NEWSROOM: Police Sexual Misconduct, Tech, Sam Liccardo, Robin Williams (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365305716/"
-          video_url="https://www.pbs.org/video/greater-boston-august-12-2014/"
-          site_url="https://www.pbs.org/video/greater-boston-august-12-2014/"
-          site_title="GREATER BOSTON: August 12, 2014 (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365313091/"
-          video_url="https://www.pbs.org/video/oklahoma-forum-mental-illness/"
-          site_url="https://www.pbs.org/video/oklahoma-forum-mental-illness/"
-          site_title="OKLAHOMA FORUM: Mental Illness (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365308174/"
-          video_url="https://www.pbs.org/video/colorado-inside-out-august-15th-2014/"
-          site_url="https://www.pbs.org/video/colorado-inside-out-august-15th-2014/"
-          site_title="COLORADO INSIDE OUT: August 15th, 2014 Edition (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/3039264729/"
-          video_url="https://www.pbs.org/video/awakenings-web-extra-ksrtwg/"
-          site_url="https://www.pbs.org/video/awakenings-web-extra-ksrtwg/"
-          site_title="SATURDAY NIGHT CINEMA: Awakenings WEB EXTRA (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/3014449168/"
-          video_url="https://www.pbs.org/video/robin-williams-1531779159/"
-          site_url="https://www.pbs.org/video/robin-williams-1531779159/"
-          site_title="PBS NEWSHOUR: Inside Robin Williams' lightning mind and creative soul (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365320526/"
-          video_url="https://www.pbs.org/video/pioneers-television-robin-williams-remembered/"
-          site_url="https://www.pbs.org/video/pioneers-television-robin-williams-remembered/"
-          site_title="PIONEERS OF TELEVISION: Robin Williams Remembered (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365320526/"
-          video_url="https://www.pbs.org/video/pioneers-television-robin-williams-remembered/"
-          site_url="https://www.pbs.org/video/pioneers-television-robin-williams-remembered/"
-          site_title="PIONEERS OF TELEVISION: Robin Williams Remembered (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365233373/"
-          video_url="https://www.pbs.org/video/pioneers-television-robin-williams-hones-his-craft/"
-          site_url="https://www.pbs.org/video/pioneers-television-robin-williams-hones-his-craft/"
-          site_title="PIONEERS OF TELEVISION: Robin Williams Hones his Craft (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365233384/"
-          video_url="https://www.pbs.org/video/pioneers-television-jonathan-winters-mentors-robin-williams/"
-          site_url="https://www.pbs.org/video/pioneers-television-jonathan-winters-mentors-robin-williams/"
-          site_title="PIONEERS OF TELEVISION: Jonathan Winters 'Mentors' Robin Williams (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365489505/"
-          video_url="https://www.pbs.org/video/blank-blank-robin-williams-masks/"
-          site_url="https://www.pbs.org/video/blank-blank-robin-williams-masks/"
-          site_title="BLANK ON BLANK: Robin Williams on Masks (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/3031776866/"
-          video_url="https://www.pbs.org/video/robin-1471vu/"
-          site_url="https://www.pbs.org/video/robin-1471vu/"
-          site_title="INSIDE THE COVER: Robin (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365305581/"
-          video_url="https://www.pbs.org/video/chicago-tonight-august-11-2014-remembering-robin-williams/"
-          site_url="https://www.pbs.org/video/chicago-tonight-august-11-2014-remembering-robin-williams/"
-          site_title="CHICAGO TONIGHT: Remembering Robin Williams (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365601348/"
-          video_url="https://www.pbs.org/video/robin-williams-didn-t-know-he-had-this-mysterious-disease-1454024744/"
-          site_url="https://www.pbs.org/video/robin-williams-didn-t-know-he-had-this-mysterious-disease-1454024744/"
-          site_title="PBS NEWSHOUR: Robin Williams didn’t know he had this mysterious disease (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365305584/"
-          video_url="https://www.pbs.org/video/chicago-tonight-august-11-2014-discussing-depression/"
-          site_url="https://www.pbs.org/video/chicago-tonight-august-11-2014-discussing-depression/"
-          site_title="CHICAGO TONIGHT: Discussing Depression (Click me Video No Work)"
-        />
-        <DisplayVideo
-          // video_url="https://player.pbs.org/viralplayer/2365308829/"
-          video_url="https://www.pbs.org/video/studio-socal-mental-illness-whats-new/"
-          site_url="https://www.pbs.org/video/studio-socal-mental-illness-whats-new/"
-          site_title="STUDIO SOCAL: Mental Illness, What's New? (Click me Video No Work)"
-        />
-        <a
-          href="https://africacheck.org/fbcheck/worst-thing-in-life-quote-not-by-robin-williams-written-for-2009-movie/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://www.bucketlistpublications.com/wp-content/uploads/2014/08/f47e31a054ada8d327c8a53626b84faf.jpg"
-            width="800"
-            alt="I Used To Think"
+      <div className="text-center">
+        <div className="add-padding-bottom pbs-videos-container">
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365305654/"
+            video_url="https://www.pbs.org/video/robin-williams-made-transformation-look-effortless-1415140089/"
+            site_url="https://www.pbs.org/video/robin-williams-made-transformation-look-effortless-1415140089/"
+            site_title="PBS NEWSHOUR: Robin Williams made transformation look effortless (Click me Video No Work)"
           />
-          <p>click here for more info</p>
-          <p>Explain about miss information and the issues of interpretation vs intent</p>
-        </a>
-        <button>
-          <Link to={HOME}>Go Back</Link>
-        </button>
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365509819/"
+            video_url="https://www.pbs.org/video/on-story-dead-poets-society-deconstructing-acclaimed-classic/"
+            site_url="https://www.pbs.org/video/on-story-dead-poets-society-deconstructing-acclaimed-classic/"
+            site_title="ON STORY Dead Poets Society: Deconstructing the Acclaimed Classic (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365308667/"
+            video_url="https://www.pbs.org/video/kqed-newsroom-education-reform-rim-fire-remembering-robin-williams/"
+            site_url="https://www.pbs.org/video/kqed-newsroom-education-reform-rim-fire-remembering-robin-williams/"
+            site_title="KQED NEWSROOM: Education Reform, Rim Fire, Remembering Robin Williams (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365839270/"
+            video_url="https://www.pbs.org/video/kqed-newsroom-police-sexual-misconduct-tech-sam-liccardo-robin-williams/"
+            site_url="https://www.pbs.org/video/kqed-newsroom-police-sexual-misconduct-tech-sam-liccardo-robin-williams/"
+            site_title="KQED NEWSROOM: Police Sexual Misconduct, Tech, Sam Liccardo, Robin Williams (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365305716/"
+            video_url="https://www.pbs.org/video/greater-boston-august-12-2014/"
+            site_url="https://www.pbs.org/video/greater-boston-august-12-2014/"
+            site_title="GREATER BOSTON: August 12, 2014 (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365313091/"
+            video_url="https://www.pbs.org/video/oklahoma-forum-mental-illness/"
+            site_url="https://www.pbs.org/video/oklahoma-forum-mental-illness/"
+            site_title="OKLAHOMA FORUM: Mental Illness (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365308174/"
+            video_url="https://www.pbs.org/video/colorado-inside-out-august-15th-2014/"
+            site_url="https://www.pbs.org/video/colorado-inside-out-august-15th-2014/"
+            site_title="COLORADO INSIDE OUT: August 15th, 2014 Edition (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/3039264729/"
+            video_url="https://www.pbs.org/video/awakenings-web-extra-ksrtwg/"
+            site_url="https://www.pbs.org/video/awakenings-web-extra-ksrtwg/"
+            site_title="SATURDAY NIGHT CINEMA: Awakenings WEB EXTRA (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/3014449168/"
+            video_url="https://www.pbs.org/video/robin-williams-1531779159/"
+            site_url="https://www.pbs.org/video/robin-williams-1531779159/"
+            site_title="PBS NEWSHOUR: Inside Robin Williams' lightning mind and creative soul (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365320526/"
+            video_url="https://www.pbs.org/video/pioneers-television-robin-williams-remembered/"
+            site_url="https://www.pbs.org/video/pioneers-television-robin-williams-remembered/"
+            site_title="PIONEERS OF TELEVISION: Robin Williams Remembered (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365320526/"
+            video_url="https://www.pbs.org/video/pioneers-television-robin-williams-remembered/"
+            site_url="https://www.pbs.org/video/pioneers-television-robin-williams-remembered/"
+            site_title="PIONEERS OF TELEVISION: Robin Williams Remembered (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365233373/"
+            video_url="https://www.pbs.org/video/pioneers-television-robin-williams-hones-his-craft/"
+            site_url="https://www.pbs.org/video/pioneers-television-robin-williams-hones-his-craft/"
+            site_title="PIONEERS OF TELEVISION: Robin Williams Hones his Craft (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365233384/"
+            video_url="https://www.pbs.org/video/pioneers-television-jonathan-winters-mentors-robin-williams/"
+            site_url="https://www.pbs.org/video/pioneers-television-jonathan-winters-mentors-robin-williams/"
+            site_title="PIONEERS OF TELEVISION: Jonathan Winters 'Mentors' Robin Williams (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365489505/"
+            video_url="https://www.pbs.org/video/blank-blank-robin-williams-masks/"
+            site_url="https://www.pbs.org/video/blank-blank-robin-williams-masks/"
+            site_title="BLANK ON BLANK: Robin Williams on Masks (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/3031776866/"
+            video_url="https://www.pbs.org/video/robin-1471vu/"
+            site_url="https://www.pbs.org/video/robin-1471vu/"
+            site_title="INSIDE THE COVER: Robin (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365305581/"
+            video_url="https://www.pbs.org/video/chicago-tonight-august-11-2014-remembering-robin-williams/"
+            site_url="https://www.pbs.org/video/chicago-tonight-august-11-2014-remembering-robin-williams/"
+            site_title="CHICAGO TONIGHT: Remembering Robin Williams (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365601348/"
+            video_url="https://www.pbs.org/video/robin-williams-didn-t-know-he-had-this-mysterious-disease-1454024744/"
+            site_url="https://www.pbs.org/video/robin-williams-didn-t-know-he-had-this-mysterious-disease-1454024744/"
+            site_title="PBS NEWSHOUR: Robin Williams didn’t know he had this mysterious disease (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365305584/"
+            video_url="https://www.pbs.org/video/chicago-tonight-august-11-2014-discussing-depression/"
+            site_url="https://www.pbs.org/video/chicago-tonight-august-11-2014-discussing-depression/"
+            site_title="CHICAGO TONIGHT: Discussing Depression (Click me Video No Work)"
+          />
+          <DisplayVideo
+            // video_url="https://player.pbs.org/viralplayer/2365308829/"
+            video_url="https://www.pbs.org/video/studio-socal-mental-illness-whats-new/"
+            site_url="https://www.pbs.org/video/studio-socal-mental-illness-whats-new/"
+            site_title="STUDIO SOCAL: Mental Illness, What's New? (Click me Video No Work)"
+          />
+          <a
+            href="https://africacheck.org/fbcheck/worst-thing-in-life-quote-not-by-robin-williams-written-for-2009-movie/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://www.bucketlistpublications.com/wp-content/uploads/2014/08/f47e31a054ada8d327c8a53626b84faf.jpg"
+              width="800"
+              alt="I Used To Think"
+            />
+            <p>click here for more info</p>
+            <p>Explain about miss information and the issues of interpretation vs intent</p>
+          </a>
+          <button>
+            <Link to={HOME}>Go Back</Link>
+          </button>
+        </div>
       </div>
     );
   }
